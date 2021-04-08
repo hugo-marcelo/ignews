d37b10c1ff1f111277453f49de21c1739a6a3212
Hugo Marcelo <hugo.silva@arquivei.com.br>
2021-04-07 22:02:16 -0300

    feat: config prismic and get posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { SignInButton } from 'components/SignInButton';
 
 import styles from './styles.module.scss';
@@ -8,10 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="logo of ig.news" />
         <nav>
-          <a className={styles.active} href="/">
-            Home
-          </a>
-          <a href="/">Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
